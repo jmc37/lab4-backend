a61b8f4d7defc07722995aa6eea5f5b06ccb3613
Joseph Chan <josephchan36@gmail.com>
2023-10-08 10:00:22 -0700

    edited routes with JSON

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const { response } = require('express');
 const http = require('http'); const url = require('url');
 const GET = 'GET'; const POST = 'POST';
-const dictionary = {}
+const dictionary = {};
+let count = 0;
+let request = 0;
+let reply;
 http.createServer(function(req,res){
     res.writeHead(200, {
         "Content-Type": "text/HTML",
@@ -9,13 +13,15 @@ http.createServer(function(req,res){
     
     });
     if(req.method === GET){
+        request += 1;
         const q = url.parse(req.url, true);
         if(q.query["term"] in dictionary){
-            res.end(`${dictionary[q.query["term"]]}`);
+            reply = `${q.query["term"]}: ${dictionary[q.query["term"]]}`;
         }
         else{
-        res.end("Item not found");
+            reply = `Request # ${request}, word ${q.query["term"]} not found!`;
         }
+        res.end(JSON.stringify(reply));
     } else if (req.method === POST) {
         let body = '';
         
@@ -25,9 +31,15 @@ http.createServer(function(req,res){
         
         req.on('end', () => {
             const data = JSON.parse(body);
-            dictionary[data.term] = data.definition
-            console.log(dictionary)
-            res.end(`Received: ${data.term} and ${data.definition} `);
-        });
+            if(data.term in dictionary){
+                reply = "Warning, item already exists"
+            }
+            else{
+                dictionary[data.term] = data.definition;
+                count += 1;
+                reply = `Request # ${count}\nNew entry recorded:\n"${data.term} : ${data.definition}"`
+            }
+            res.end(JSON.stringify(reply));
+        })
     }
 }).listen(8888);
